8a6894e52cfa43fa13479c0197a85b9dc75de73f
agiUnderground <simplecodemaster@gmail.com>
2023-01-21 13:39:42 +0300

    Google search page basic support.

---
+++ foxinthemiddle.js
@@ -0,0 +1,6 @@
+/*
+Center the content of the Google search page.
+*/
+document.getElementById("rcnt").style.justifyContent = "center"
+document.getElementById("rcnt").style.maxWidth = "100vw"
+document.getElementById("center_col").style.marginLeft = "0px"
