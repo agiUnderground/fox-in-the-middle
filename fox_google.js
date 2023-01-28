9932670fb3daa5fd85fa112d2de90d7d712aac51
agiUnderground <simplecodemaster@gmail.com>
2023-01-28 13:57:59 +0300

    Bugfixes related to Google.com features snippets.

--- fox_google.js
+++ fox_google.js
@@ -3,4 +3,12 @@ Center the content of the Google search page.
 */
 document.getElementById("rcnt").style.justifyContent = "center";
 document.getElementById("rcnt").style.maxWidth = "100vw";
-document.getElementById("center_col").style.marginLeft = "0px";
+document.getElementById("rcnt").style.flexFlow = "column";
+document.getElementById("center_col").style.margin = "auto";
+const collection = document.getElementsByClassName("M8OgIe")
+for (let i = 0; i < collection.length; i++) {
+  collection[i].style.margin = "auto";
+}
+document.getElementById("rhs").style.position = "absolute";
+document.getElementById("rhs").style.marginLeft = "69%";
+document.getElementById("rhs").style.top = "170px";
